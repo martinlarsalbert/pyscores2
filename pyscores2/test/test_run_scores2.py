0792d06a61fa09578c0cffecefb8a45026daadd4
Martin Alexandersson <marale@kth.se>
2020-09-09 15:28:51 +0200

    Fixed the example temp.in file

--- pyscores2/test/test_run_scores2.py
+++ pyscores2/test/test_run_scores2.py
@@ -32,6 +32,6 @@ def test_get_result_no_run(calculation):
 def test_get_result(calculation):
     calculation.run(indata_file_path=pyscores2.test.indata_path)
     added_resistance_RAOs = calculation.getResult()
-    assert added_resistance_RAOs[181][0].responses == 0.975603955801746
+    assert added_resistance_RAOs[20][0].responses[0] == 0.5750000000000001
 
 
